b7f9c8aafaaa5b93b8aaa159528f31349ad2785b
mbXdev <63065904+mbXdev@users.noreply.github.com>
2020-05-02 20:43:34 +0200

    Creating sample pages

---
+++ Views/Warning/404.php
@@ -0,0 +1,7 @@
+<h2>La page demandée n'a pas été trouvée</h2>
+<p>La page à laquelle vous tentez d'accéder est introuvable. Vous pouvez contacter l'administrateur du site si vous pensez qu'il s'agit d'un bug.</p>
+<br />
+<br />
+<p>Contact: ...</p>
+
+

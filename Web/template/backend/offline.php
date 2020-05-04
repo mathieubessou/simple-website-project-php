315c414b5e0e8486ff3d45f5cb92513323e4d121
mbXdev <63065904+mbXdev@users.noreply.github.com>
2020-05-04 05:49:19 +0200

    Added a meta "robots" tag to the file /template/backend/offline.php

--- Web/template/backend/offline.php
+++ Web/template/backend/offline.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
 		<title>Service Unavailable</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="noindex">
 		<link rel="icon" type="image/png" href="/template/images/icon.png" />
     </head>
 

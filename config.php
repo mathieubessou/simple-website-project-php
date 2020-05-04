4a8367331d0ffacc2e8f69c8ce6bad4e9a7cd0bc
mbXdev <63065904+mbXdev@users.noreply.github.com>
2020-05-04 05:36:21 +0200

    Added the "Username" setting to the config.php file.

--- config.php
+++ config.php
@@ -31,6 +31,12 @@ const CONFIG = [
         // Ca évite aussi que des personnes tentent de se connecter et finissent par bloquer l'accès au Backend.
         "SpecialPath" => "path-for-connection",
 
+        // Définit l'identifiant pour se connecter à l'administration (Backend).
+        // Ceci est une protection supplémentaire. Si personne d'autre que l'administrateur connait cet identifiant, il sera plus difficile d'accéder au Backend (à l'administration) pour un autre utilisateur.
+        // Il est conseillé de le personnaliser pour qu'il soit encore plus difficile pour une tiers personne de se connecter au Backend.
+        // Pour être valide, l'identifiant contenir au moins 3 caractères.
+        "Username" => "admin",
+
         // Définit le mot de passe pour accéder à l'administration (Il faut utiliser un mot de passe long pour plus de sécurité.)
         // Attention! Il est conseillé de créer un mot de passe réservé exclusivement pour la connexion au backend de ce projet (site).
         // Pour être valide, le mot de passe doit contenir au moins: 10 caractères et au moins 1 de chaque type suivant: minuscule, majuscule, chiffre.

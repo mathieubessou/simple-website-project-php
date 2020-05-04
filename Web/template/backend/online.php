8deaba47b46a83fb5483eff28efc0292e5781e36
mbXdev <63065904+mbXdev@users.noreply.github.com>
2020-05-04 05:48:37 +0200

    Added several meta and other tags to the /template/backend/online.php file

--- Web/template/backend/online.php
+++ Web/template/backend/online.php
@@ -10,7 +10,11 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-
+		<meta charset="utf-8">
+		<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="noindex">
+		<link rel="icon" type="image/png" href="/template/images/icon.png" />
 	</head>
 
 	<body>
